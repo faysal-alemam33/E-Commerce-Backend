8de88ab4a3bedf5479d18bbc016717256f8e4994
faysal <fesalali3369@gmail.com>
2024-12-15 13:18:58 +0200

    push project 1st commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Commerce-Backend
+This is E-commerce project using spring boot and java
---
+++ src/main/resources/db/migration/V1__Initial_script.sql
@@ -0,0 +1,6 @@
+create table user(
+
+    id integer primary key,
+    name varchar(255) not null,
+    email varchar(255)not null
+);
